289bf9c4c78e5a5066f3da6f167f39b934fc7238
YiHsuan85 <yihsuanwu85@gmail.com>
2026-05-24 09:07:57 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/react-refresh' // 或 @vitejs/plugin-react
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Turtle-EggTart/', // 必須與你的 GitHub 儲存庫名稱完全一致，前後都要有斜線
 })
